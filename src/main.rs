75964f42af4b3806541eacdce9cb41a1106d1e32
CanIBeFii <filipe250502@gmail.com>
2024-03-22 15:51:49 +0000

    feat: change program to use termion input and events

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-fn to_ctrl_byte(c: char) -> u8 {
-	let byte = c as u8;
-
-	byte & 0b0001_1111
-}
-
 fn close_program(error: std::io::Error) {
-	panic!(error);
+	panic!("{}", error);
 }
 
 fn main() {
 	let _stdout =stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-		match b {
-			Ok(b) => {
-				let letter = b as char;
-
-				if letter.is_control() {
-					println!("{:#b} \r", b);
-				} else {
-					println!("{} ({:#b})\r", letter, b);
+	for key in io::stdin().keys() {
+		match key {
+			Ok(key) => match key {
+				Key::Char(c) => {
+					if c.is_control() {
+						println!("{:#b} \r", c as u8);
+					} else {
+						println!("{} ({:?})\r", c, c as u8);
+					}
 				}
-
-				if b == to_ctrl_byte('q') {
-					break;
-				}
+				Key::Ctrl('q') => break,
+				_ => println!("{:?}\r", key),
 			}
-			Err(error) => close_program(error)
+			Err(error) => close_program(error),
 		}
 	}
 }
