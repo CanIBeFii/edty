eef40b5e5c6ee7b5ff1cbe628e052a9e74011c21
CanIBeFii <filipe250502@gmail.com>
2024-03-22 15:31:40 +0000

    feat: program prints bytes off the input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn to_ctrl_byte(c: char) -> u8 {
+	let byte = c as u8;
+
+	byte & 0b0001_1111
+}
+
 fn main() {
 	let _stdout =stdout().into_raw_mode().unwrap();
 
@@ -9,11 +15,11 @@ fn main() {
 		let letter = b as char;
 
 		if letter.is_control() {
-			println!("{:?} \r", b);
+			println!("{:#b} \r", b);
 		} else {
-			println!("{} ({:?})\r", letter, b);
+			println!("{} ({:#b})\r", letter, b);
 		}
-		if letter == 'q' {
+		if b == to_ctrl_byte('q') {
 			break;
 		}
 	}
